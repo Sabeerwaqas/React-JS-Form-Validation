dca99f664ef71aef74ae7a2f382e1e7b820bab3a
Sabeerwaqas <sabeerwaqas050@gmail.com>
2024-04-06 02:49:20 +0500

    [Add: MUI]

---
+++ src/pages/registeration/Registeration.js
@@ -0,0 +1,11 @@
+import React, {useRef, useState, useEffect} from 'react'
+
+const USER_REGEX = /^[a-zA-Z][a-zA-Z0-9-_]{3,23}$/;
+
+const Registeration = () => {
+  return (
+    <div>Registeration</div>
+  )
+}
+
+export default Registeration

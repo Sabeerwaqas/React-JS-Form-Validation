14609950195e93b5f2c6c4223ab672724d5e7cee
Sabeerwaqas <sabeerwaqas050@gmail.com>
2024-04-06 15:30:27 +0500

    [Add: States for 'user name', 'user password', and 'user valid password']

--- src/pages/registeration/Registeration.js
+++ src/pages/registeration/Registeration.js
@@ -1,11 +1,34 @@
-import React, {useRef, useState, useEffect} from 'react'
-
-const USER_REGEX = /^[a-zA-Z][a-zA-Z0-9-_]{3,23}$/;
+import React, { useRef, useState, useEffect } from "react";
 
 const Registeration = () => {
-  return (
-    <div>Registeration</div>
-  )
-}
+  // const USER_REGEX = /^[a-zA-Z][a-zA-Z0-9-_]{3,23}$/;
+  // const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=*[0-9])(?=.*[!@#$%]).{8, 24}$/
 
-export default Registeration
+  // To reference a user's input
+  const userRef = useRef();
+
+  // To show error
+  const errRef = useRef();
+
+  // For name
+
+  const [user, setUser] = useState("");
+  const [userName, setUserName] = useState("");
+  const [userFocus, setUserFocus] = useState("");
+
+  // For password
+
+  const [pwd, setPwd] = useState("");
+  const [userPwd, setUserPwd] = useState("");
+  const [pwdFocus, setPwdFocus] = useState("");
+
+  // For valid password
+
+  const [validPwd, serValidPwd] = useState("");
+  const [userValidPwd, setUserValidPwd] = useState("");
+  const [validPwdFocus, setValidPwdFocus] = useState("");
+
+  return <div>Registeration</div>;
+};
+
+export default Registeration;
